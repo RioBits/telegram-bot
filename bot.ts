98ab83214c4bf433d58c9607d901f20543978c75
riobits <23579928+RioBits@users.noreply.github.com>
2021-11-12 23:33:48 +0300

    Add: shortcut command to show btc & eth & doge prices in one message

--- bot.ts
+++ bot.ts
@@ -8,13 +8,6 @@ const token = process.env.TOKEN!
 
 const bot = new TelegramBot(token, { polling: true })
 
-bot.onText(/\/echo (.+)/, (msg, match) => {
-  const chatId = msg.chat.id
-  const resp = match![1]
-
-  bot.sendMessage(chatId, resp)
-})
-
 interface apiResponse {
   data: {
     amount: string
@@ -23,15 +16,41 @@ interface apiResponse {
   }
 }
 
+bot.onText(/\/price/, async (msg) => {
+  const chatId = msg.chat.id
+
+  const {
+    data: {
+      data: { amount: BtcAmount },
+    },
+  } = await axios.get<apiResponse>(
+    `https://api.coinbase.com/v2/prices/BTC-USD/spot`
+  )
+  const {
+    data: {
+      data: { amount: EthAmount },
+    },
+  } = await axios.get<apiResponse>(
+    `https://api.coinbase.com/v2/prices/ETH-USD/spot`
+  )
+  const {
+    data: {
+      data: { amount: DogeAmount },
+    },
+  } = await axios.get<apiResponse>(
+    `https://api.coinbase.com/v2/prices/DOGE-USD/spot`
+  )
+
+  return bot.sendMessage(
+    chatId,
+    `BTC => USD: ${BtcAmount}\nETH => USD: ${EthAmount}\nDOGE => USD: ${DogeAmount}\n`
+  )
+})
+
 bot.onText(/\/price (.+)/, async (msg, match) => {
   const chatId = msg.chat.id
   const resp = match![1].toLowerCase()
   let currency: string
-
-  // Joke (not real)
-  if (resp === 'redeast' || resp === 're' || resp === 'red') {
-    return bot.sendMessage(chatId, 'RE => USD: 1267438.87')
-  }
 
   switch (resp) {
     case 'bitcoin':
@@ -66,9 +85,3 @@ bot.onText(/\/price (.+)/, async (msg, match) => {
     )
   }
 })
-
-// bot.on('message', (msg) => {
-//   const chatId = msg.chat.id
-
-//   bot.sendMessage(chatId, 'Received your message')
-// })
