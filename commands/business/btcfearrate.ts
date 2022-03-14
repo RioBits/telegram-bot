fc995f7beb5038aa1a6fd3a6458c6c0e73bc4672
riobits <23579928+RioBits@users.noreply.github.com>
2022-03-14 19:33:44 +0300

    New command

---
+++ commands/business/btcfearrate.ts
@@ -0,0 +1,18 @@
+import axios from 'axios'
+import Command from '../../types/command'
+
+const command: Command = {
+  name: 'btcfearrate',
+  description: 'search for a companies asset name.',
+  aliases: ['btcfr'],
+  async execute(bot, chatId) {
+    const { data } = await axios.get('https://api.alternative.me/fng')
+    bot.sendMessage(
+      chatId,
+      `<b>Bitcoin Fear Rate 🔎:</b>\n<em>${data.data[0].value_classification}</em>`,
+      { parse_mode: 'HTML' }
+    )
+  },
+}
+
+export default command
